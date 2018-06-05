8319e8c4069152c0fd5428fbfda24334867c8883
Станислав Тищенко <tishchenko@MacBook-Pro-Stanislav.local>
2018-06-05 11:47:27 +0300

    get data

---
+++ GetDataFromStackOverflow/AuthorVC.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorVC.swift
+//  GetDataFromStackOverflow
+//
+//  Created by Станислав Тищенко on 05.06.2018.
+//  Copyright © 2018 Станислав Тищенко. All rights reserved.
+//
+
+import UIKit
+
+class AuthorVC: UIViewController {
+    var authorLink: URL?
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Author"
+        let request = URLRequest(url: authorLink!)
+        webView.loadRequest(request)
+    }
+}
---
+++ GetDataFromStackOverflow/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  GetDataFromStackOverflow
+//
+//  Created by Станислав Тищенко on 04.06.2018.
+//  Copyright © 2018 Станислав Тищенко. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    var dataToNextScreen = [Items]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let currentTime = Date()
+        let currentTimeInSeconds = Int(currentTime.timeIntervalSince1970 + 10800)
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: currentTime)
+        let minutes = calendar.component(.minute, from: currentTime)
+        let seconds = calendar.component(.second, from: currentTime)
+        let todayMidnight = currentTimeInSeconds - hour * 3600 + minutes * 60 + seconds
+        let param = ["fromdate": todayMidnight, "todate": currentTimeInSeconds, "key": "7DSNIn7QrxoUMbO62ggDeg((", "tagged":"swift4"] as [String : Any]
+
+        request("https://api.stackexchange.com/2.2/questions/no-answers?order=desc&sort=activity&site=stackoverflow&filter=!WaV5.9c(PIZ0s2P3DZdZXxN5mRC0vshdCL85j.4E-f)rmwJ()KTr6vJI762CKo2i_vs3*iqx.1LWjaixTj5PP", parameters: param).responseJSON { response in
+            let inputData = response.data
+            let decoder = JSONDecoder()
+            let stat = try! decoder.decode(Stat.self, from: inputData!)
+            self.dataToNextScreen = stat.items
+            //print(stat.items[0].link)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toTableWithData" {
+            let destination = segue.destination as! DataTableVC
+            destination.arrayWithData = dataToNextScreen
+        }
+    }
+}
+
+
